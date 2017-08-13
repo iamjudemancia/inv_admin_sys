505f102afa71feb5abf8c289b9199f357b2f900f
Rey <rmmedillo@gmail.com>
2017-08-13 21:21:03 +0800

    add api routes

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -569,22 +569,34 @@ require get_parent_theme_file_path( '/inc/icon-functions.php' );
 /**
  * This is our callback function that embeds our phrase in a WP_REST_Response
  */
-function prefix_get_endpoint_phrase() {
+function get_questions() {
     // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
     return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+}
+
+function get_answers() {
+    // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
+    return rest_ensure_response( 'Get answers route...' );
 }
  
 /**
  * This function is where we register our routes for our example endpoint.
  */
-function prefix_register_example_routes() {
+function prefix_register_routes() {
     // register_rest_route() handles more arguments but we are going to stick to the basics for now.
-    register_rest_route( 'questions/v1', '/phrase', array(
+    register_rest_route( 'api/v1', '/questions', array(
         // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
         'methods'  => WP_REST_Server::READABLE,
         // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
-        'callback' => 'prefix_get_endpoint_phrase',
+        'callback' => 'get_questions',
+    ) );
+
+    register_rest_route( 'api/v1', '/answers', array(
+        // By using this constant we ensure that when the WP_REST_Server changes our readable endpoints will work as intended.
+        'methods'  => WP_REST_Server::READABLE,
+        // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+        'callback' => 'get_answers',
     ) );
 }
  
-add_action( 'rest_api_init', 'prefix_register_example_routes' );
+add_action( 'rest_api_init', 'prefix_register_routes' );
